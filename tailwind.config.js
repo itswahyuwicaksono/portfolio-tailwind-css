10064be52c1daeb03ad8593b07ca76dc7ab4889a
Wahyu Wicaksono <wahyuwicaksono93@gmail.com>
2022-09-20 11:07:29 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['index.html'],
+	darkMode: 'class',
+	theme: {
+		container: {
+      		center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem',
+			}
+    	},
+    	extend: {
+      		colors: {
+        		primary: '#14b8a6',
+        		secondary: '#64748b',
+        		dark: '#0f172a',
+      		},
+      		screens: {
+        		'2xl': '1600px',
+      		}
+    	},
+  	},
+  	plugins: [],
+}
